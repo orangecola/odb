8a0fe9bf311a6dc4448fef6eafffd660892857b0
orangecola <orangecola3@gmail.com>
2023-02-02 10:38:33 +0800

    rename gitignore, update odb to time zone

--- odb.py
+++ odb.py
@@ -1,9 +1,12 @@
 import json, urllib3
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def odb(event, context):
 	http = urllib3.PoolManager()
-	raw_html = http.request('GET', 'https://api.experience.odb.org/devotionals/v2?site_id=1&status=publish&country=SG&on=' + str(datetime.now().month) + '-' + str(datetime.now().day) + '-' + str(datetime.now().year))
+	now = datetime.now()
+	now += timedelta(hours=8)
+	print(now)
+	raw_html = http.request('GET', 'https://api.experience.odb.org/devotionals/v2?site_id=1&status=publish&country=SG&on=' + str(now.month) + '-' + str(now.day) + '-' + str(now.year))
 	req = json.loads(raw_html.data)
 	return {
 		"statusCode": 302,
@@ -14,4 +17,4 @@ def odb(event, context):
 	}
 
 if __name__ == '__main__':
-	print(odb())
+	print(odb([], []))
